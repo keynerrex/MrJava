2a1c3a1acc87a823c8dec227a3169d5e74bbeb98
keynerrex <keyneroliveros24@gmail.com>
2022-11-29 22:42:20 -0500

    ERROR Solution given: start another project (No error :D )
    An error occurred while cloning repository to another linux distro.
    Error code:
    ant -f /home/keynerrex/NetBeansProjects/mrJava/part2/Poo -Dnb.internal.action.name=run.single -Djavac.includes=classes/Main.java -Drun.class=classes.Main run-single
    init:
    Deleting: /home/keynerrex/NetBeansProjects/mrJava/part2/Poo/build/built-jar.properties
    deps-jar:
    Updating property file: /home/keynerrex/NetBeansProjects/mrJava/part2/Poo/build/built-jar.properties
    Compiling 1 source file to /home/keynerrex/NetBeansProjects/mrJava/part2/Poo/build/classes
    
    Solution given: start another project (No error :D )

---
+++ parte 5/nuevoAts/src/ClasesYObjetos/NewClass.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClasesYObjetos;
+
+/**
+ *
+ * @author keynerrex
+ */
+public class NewClass {
+    public static void main(String[] args) {
+
+    Operacion op = new Operacion();
+
+    op.leerNumeros ();
+
+    op.suma ();
+
+    op.resta ();
+
+    op.multiplicacion ();
+
+    op.division ();
+
+    op.mostrarResultados ();
+}
+
+}
