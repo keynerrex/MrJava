590b9b3d710dda66eec2e888457dfe4a3f0a1de8
keynerrex <keyneroliveros24@gmail.com>
2022-12-13 00:04:55 -0500

    Java Web
    FULL ERRORS D:

--- parte 7/Java_Mysql/src/main/java/com/oregoom/mensajes/DeleteMysql.java
+++ parte 7/Java_Mysql/src/main/java/com/oregoom/mensajes/DeleteMysql.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DeleteMysql {
-
+    
     public static void main(String[] args) throws SQLException {
         SelectMysql.listarRegistros();
         borrarRegistros(8);
