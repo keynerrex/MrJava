a387203edfb296d17134dd13bd819c0a172d71df
keynerrex <keyneroliveros24@gmail.com>
2023-02-21 22:15:58 -0500

    ajustes nombres

---
+++ peluqueria/Peluqueria/build/generated-sources/ap-source-output/com/mycompany/veterinariacanina/logica/Dueno_.java
@@ -0,0 +1,15 @@
+package com.mycompany.veterinariacanina.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-02-21T22:06:59", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Dueno.class)
+public class Dueno_ { 
+
+    public static volatile SingularAttribute<Dueno, String> celDueno;
+    public static volatile SingularAttribute<Dueno, String> nombre;
+    public static volatile SingularAttribute<Dueno, Integer> id_dueno;
+
+}
---
+++ peluqueria/Peluqueria/build/generated-sources/ap-source-output/com/mycompany/veterinariacanina/logica/Mascota_.java
@@ -0,0 +1,21 @@
+package com.mycompany.veterinariacanina.logica;
+
+import com.mycompany.veterinariacanina.logica.Dueno;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-02-21T22:06:59", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Mascota.class)
+public class Mascota_ { 
+
+    public static volatile SingularAttribute<Mascota, Integer> num_cliente;
+    public static volatile SingularAttribute<Mascota, String> raza;
+    public static volatile SingularAttribute<Mascota, String> calergico;
+    public static volatile SingularAttribute<Mascota, String> color;
+    public static volatile SingularAttribute<Mascota, String> observaciones;
+    public static volatile SingularAttribute<Mascota, String> atencion_especial;
+    public static volatile SingularAttribute<Mascota, String> nombre;
+    public static volatile SingularAttribute<Mascota, Dueno> unDueno;
+
+}
